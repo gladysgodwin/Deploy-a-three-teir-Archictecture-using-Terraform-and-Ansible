da7ac640596b37cf6dcb8aef099da578935a10cf
Gladys Oviawe <99274632+gladysgodwin@users.noreply.github.com>
2023-01-25 04:25:27 +0100

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -1,7 +1,7 @@
 resource "aws_autoscaling_group" "gladys_asg" {
   count = "${length(aws_subnet.love-private-subnt.id)}"
   
-  vpc_zone_identifier = "${aws_subnet.love-private-subnt[count.index]}"
+  vpc_zone_identifier = "${element(aws_subnet.love-private-subnt.*.id, count.index)}"
   desired_capacity    = 3
   max_size            = 3
   min_size            = 1
