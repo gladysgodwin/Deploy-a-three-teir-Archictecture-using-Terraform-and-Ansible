92d8a7312da744f2f4ce4bb9c0cce09a6d4fcea2
Gladys Oviawe <99274632+gladysgodwin@users.noreply.github.com>
2023-01-25 05:28:06 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,13 @@ output "vpc_id" {
   description = "ID of project VPC"
   value       = aws_vpc.love_vpc.id
 }
+/*
 output "my_lb_arn" {
   description = "My Load Balancer arn"
   value       = aws_lb.my-alb[count.index]
-  # aws_lb.my-alb.arn
+  aws_lb.my-alb.arn
 }
-
+*/
 output "public-ip" {
   description = "My public ip"
   value       =  aws_autoscaling_group.gladys_asg.*.public_ip
