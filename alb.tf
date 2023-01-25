f12480e2e4d06cc850021688fceefddc4090fe2c
Gladys Oviawe <99274632+gladysgodwin@users.noreply.github.com>
2023-01-25 05:51:25 +0100

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,20 +1,15 @@
 #Create Load Balancer
 
 resource "aws_lb" "my-alb" {
-  count          = "${length(var.subnet_cidrs_public)}"
-  
   name               = "my-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.my_alb_sg.id]
-  subnets            = aws_subnet.love-public-subnt[count.index]
+  subnets            = var.pub-sub-id
 }
 
 resource "aws_lb_listener" "lb_lst" {
-  count          = "${length(var.subnet_cidrs_public)}"
-  
-  load_balancer_arn = aws_lb.my-alb[count.index]
-  #aws_lb.my-alb.arn
+  load_balancer_arn = aws_lb.my-alb.arn
   port              = "80"
   protocol          = "HTTP"
 
@@ -25,10 +20,7 @@ resource "aws_lb_listener" "lb_lst" {
 }
 
 resource "aws_lb_listener_rule" "dom-rule" {
-  count          = "${length(var.subnet_cidrs_public)}"
-  
-  listener_arn = aws_lb_listener.lb_lst[count.index]
-#aws_lb_listener.lb_lst.arn
+  listener_arn = aws_lb_listener.lb_lst.arn
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.my-alb-tg.arn
