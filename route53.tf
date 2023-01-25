d76912c99f83b62c872a5fbdd9f3e5403e9d2051
Gladys Oviawe <99274632+gladysgodwin@users.noreply.github.com>
2023-01-25 05:43:11 +0100

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -2,18 +2,14 @@ resource "aws_route53_zone" "my_hosted_zone" {
   name = var.domain
 }
 
-resource "aws_route53_record" "terraform-test" {
-  count          = "${length(var.subnet_cidrs_public)}"
-  
+resource "aws_route53_record" "terraform-test" {  
   zone_id = aws_route53_zone.my_hosted_zone.zone_id
   name    = var.a-record
   type    = "A"
 
   alias {
-    name                   = aws_lb.my-alb[count.index]
-    #aws_lb.my-alb.dns_name
-    zone_id                = aws_lb.my-alb[count.index]
-    #aws_lb.my-alb.zone_id
+    name                   = aws_lb.my-alb.dns_name
+    zone_id                = aws_lb.my-alb.zone_id
     evaluate_target_health = true
   }
 }
