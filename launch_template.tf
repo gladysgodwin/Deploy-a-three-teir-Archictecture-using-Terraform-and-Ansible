acf27ed6ef6851406724edbe7e287d50ecc0edbc
Gladys Oviawe <99274632+gladysgodwin@users.noreply.github.com>
2023-01-25 04:05:39 +0100

    Update launch_template.tf

--- launch_template.tf
+++ launch_template.tf
@@ -2,7 +2,15 @@ resource "aws_launch_template" "gladys_asg" {
   name_prefix   = "gladys_asg"
   image_id      = var.ami
   instance_type = var.instance_type
+  key_name      = "mykey"
   tags = {
     Name = "my_instances"
   }
+  depends_on = [
+aws_key_pair.mykeypair
+]
+}
+resource "aws_key_pair" "mykeypair" {
+key_name   = "mykey"
+public_key = var.pubssh-key
 }
