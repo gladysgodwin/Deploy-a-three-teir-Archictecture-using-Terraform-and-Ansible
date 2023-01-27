0715e0c68dd810aea68e9cb066d29cd8027ca3f3
Gladys Oviawe <99274632+gladysgodwin@users.noreply.github.com>
2023-01-27 10:31:43 +0100

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -10,7 +10,7 @@ resource "aws_autoscaling_group" "gladys_asg" {
     version = "$Latest"
   }
   provisioner "local-exec" {
-    command = "echo ${self.private_ip} >> host-inventory"
+    command = "echo ${self.public_ip} >> host-inventory"
   }
   /*
   provisioner "local-exec" {
