f07c68d1a63ba10b473505258292d77980ea48ff
Gladys Oviawe <99274632+gladysgodwin@users.noreply.github.com>
2023-01-27 10:12:28 +0100

    Update provisioner.tf

--- provisioner.tf
+++ provisioner.tf
@@ -9,11 +9,12 @@ provisioner “remote-exec” {
 inline = [
 amazon-linux-extras install epel yum install ansible git -y,
 ]
-*/
+
 
 provisioner “local-exec” {
   command = sudo echo "${self.public_ip}" > /root/Deploy-a-three-teir-Archictecture-using-Terraform-and-Ansible/host-inventory
 }
+*/
 /*
 provisioner “local-exec” {
   command = “sudo ansible-playbook -i host-inventory –private-key=/root/.ssh/mykey.pem main.yml”,
