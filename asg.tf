f20833cc29e7c74ef63123d3f263f56906c37b6c
Gladys Oviawe <99274632+gladysgodwin@users.noreply.github.com>
2023-01-27 11:03:14 +0100

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -1,17 +1,15 @@
 resource "aws_autoscaling_group" "gladys_asg" {
   vpc_zone_identifier =  var.pub-sub-id
-  desired_capacity    = 2
-  max_size            = 2
-  min_size            = 1
+  desired_capacity    = 0
+  max_size            = 0
+  min_size            = 0
   target_group_arns   = ["${aws_lb_target_group.my-alb-tg.arn}"]
 
   launch_template {
     id      = aws_launch_template.gladys_asg.id
     version = "$Latest"
   }
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} >> public_ip.txt"
-  }
+  
   /*
   provisioner "local-exec" {
     command = 
