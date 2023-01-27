016a2af7747f9b2d6d2db825e44ed61d0a404482
Gladys Oviawe <99274632+gladysgodwin@users.noreply.github.com>
2023-01-27 12:07:59 +0100

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -4,13 +4,18 @@ resource "aws_autoscaling_group" "gladys_asg" {
   max_size            = 0
   min_size            = 0
   target_group_arns   = ["${aws_lb_target_group.my-alb-tg.arn}"]
-
+   
   launch_template {
     id      = aws_launch_template.gladys_asg.id
     version = "$Latest"
   }
-  
-  /*
+   /*
+  resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  count = 5
+  vpc_security_group_ids = v
+ 
   provisioner "local-exec" {
     command = 
     sudo echo "${self.public_ip}" > /root/Deploy-a-three-teir-Archictecture-using-Terraform-and-Ansible/host-inventory
